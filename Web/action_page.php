14c9e04f3872af290c2959a28fd4f509afd66fce
ESQ0001 <aleesq97@gmail.com>
2018-02-27 22:15:31 +0100

    Add files via upload

--- Web/action_page.php
+++ Web/action_page.php
@@ -1,26 +1,24 @@
 <html>
 <body>
-
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
  <?php
-$servername = http://es.000webhost.com/;
-$username = uname;
-$password = psw;
 
+	/*private $host = "localhost";
+    private $dbname = "id4835931_workforce";
+    private $username = "id4835931_admin";
+    private $password = "HardPassword";*/
+    
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+$conn = new mysqli("localhost", "id4835931_admin", "HardPassword", "id4835931_workforce");
 
-$sql = SELECT * FROM `Usuario` WHERE correo=uname && contraseña=psw;
+$sql = 'SELECT * FROM `Usuario` WHERE correo=$_POST["uname"] && contraseña=$_POST["psw"]';//query sql donde uname y psw son los parametros que hemos pasado
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-header (header('Location: webWorkforce.html');)
+('Location: webWorkforce.html');//redirigimos a la bbdd del workforce
 }else{
-	echo "Usuario desconocido";
+	echo "Usuario desconocido";//en otro caso es porque no existe el usuario
 }
 ?> 
 
